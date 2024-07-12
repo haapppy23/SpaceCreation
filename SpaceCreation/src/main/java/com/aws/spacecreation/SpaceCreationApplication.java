390e0f00227810e815a7940948bcca7007795479
Gimminu <jingjing1254@naver.com>
2024-07-12 15:41:18 +0900

    최초 양식

---
+++ SpaceCreation/src/main/java/com/aws/spacecreation/SpaceCreationApplication.java
@@ -0,0 +1,13 @@
+package com.aws.spacecreation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceCreationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpaceCreationApplication.class, args);
+    }
+
+}
---
+++ SpaceCreation/src/test/java/com/aws/spacecreation/SpaceCreationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws.spacecreation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceCreationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
